b5f5e70d7506e599bb7f20a3f767cd1dc462b0e1
xlocaste <irfandi.di122@gmail.com>
2024-11-08 10:54:03 +0700

    feat: model reimbursement

--- app/Models/Reimbursement.php
+++ app/Models/Reimbursement.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reimbursement extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = "reimbursements";
+
+    protected $fillable = [
+        'user_id',
+        'tanggal',
+        'kategori',
+        'deskripsi',
+        'jumlah',
+        'status',
+        'tanggal_approval',
+        'approval_by',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
